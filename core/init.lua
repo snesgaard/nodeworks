0bda60aa3b1ce54ef0774cb38814f6147f2b4fb0
Sebastian Jensen <s.nesgaard@gmail.com>
2021-09-06 17:03:24 +0200

    Draw node included

--- core/init.lua
+++ core/init.lua
@@ -15,6 +15,7 @@ vec2 = require(BASE .. ".vec2")
 particles = require(BASE .. ".particles")
 ease = require(BASE .. ".ease")
 bump_debug = require(BASE .. ".bump_debug")
+draw_node = require(BASE .. ".draw_node")
 
 atlas_cache = {}
 function get_atlas(path)
