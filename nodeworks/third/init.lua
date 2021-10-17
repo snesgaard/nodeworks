e9eb76577b90b8d767fb8d57f3cec22b8f457ffd
Sebastian Jensen <s.nesgaard@gmail.com>
2021-10-17 20:07:37 +0200

    added json as third lib

--- nodeworks/third/init.lua
+++ nodeworks/third/init.lua
@@ -4,5 +4,6 @@ return {
     knife = {
         test = require(PATH .. ".knife.knife.test"),
     },
-    bump = require(PATH .. ".bump.bump")
+    bump = require(PATH .. ".bump.bump"),
+    json = require(PATH .. ".json")
 }
