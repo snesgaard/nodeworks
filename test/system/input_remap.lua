8287cf28b69ae410cc4139630ffdb856949cd084
Sebastian Jensen <s.nesgaard@gmail.com>
2022-02-06 10:24:22 +0100

    Fixed input remap test

--- test/system/input_remap.lua
+++ test/system/input_remap.lua
@@ -13,30 +13,33 @@ function left_right_observer.input_released(world, pool, input)
     world:singleton()[input] = v - 1
 end
 
+local scene = {}
+
 T("input_remap", function(T)
     local world = nw.ecs.world{nw.system.input_remap, left_right_observer}
+    local ctx = world:push(scene):find(scene)
 
     local keyboard_map = {a = "left", d = "right"}
-    nw.system.input_remap.set_keyboard_map(world, keyboard_map)
+    nw.system.input_remap.set_keyboard_map(ctx, keyboard_map)
 
     T("keyboard", function(T)
         for key, input in pairs(keyboard_map) do
             world("keypressed", key)
-            T:assert(world:singleton()[input] == 1)
+            T:assert(ctx:singleton()[input] == 1)
             world("keyreleased", key)
-            T:assert(world:singleton()[input] == 0)
+            T:assert(ctx:singleton()[input] == 0)
         end
     end)
 
     local gamepad_map = {x = "left", y = "right"}
-    nw.system.input_remap.set_gamepad_button_remap(world, gamepad_map)
+    nw.system.input_remap.set_gamepad_button_remap(ctx, gamepad_map)
 
     T("gamepad", function(T)
         for button, input in pairs(gamepad_map) do
             world("gamepadpressed", "joystick", button)
-            T:assert(world:singleton()[input] == 1)
+            T:assert(ctx:singleton()[input] == 1)
             world("gamepadreleased", "joystick", button)
-            T:assert(world:singleton()[input] == 0)
+            T:assert(ctx:singleton()[input] == 0)
         end
     end)
 end)
