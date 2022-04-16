accc34d880617f263d4e872a51cf4f5d5cb09c3f
Sebastian Jensen <SJensen4@its.jnj.com>
2022-04-16 21:46:26 +0200

    ability to clear drawers

--- nodeworks/core/layer.lua
+++ nodeworks/core/layer.lua
@@ -34,7 +34,16 @@ end
 local function invoke(func, ...) return func(...) end
 
 function layer:draw()
-    for _, draw_args in ipairs(self.state) do invoke(unpack(draw_args)) end
+    for _, draw_args in ipairs(self.state) do
+        if invoke(unpack(draw_args)) then return true end
+    end
+
+    return false
+end
+
+function layer:clear()
+    self.state = list()
+    return self
 end
 
 local layer_manager = {}
@@ -48,6 +57,10 @@ function layer_manager:__call(key)
     local l = self[key]
     if not l then self[key] = layer.create() end
     return self[key]
+end
+
+function layer_manager:clear()
+    for key, layer in pairs(self) do layer:clear() end
 end
 
 return layer_manager.create
