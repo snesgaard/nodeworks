13c71a19d83c461d8250f4cafbc4062771b8a5bd
Sebastian Jensen <s.nesgaard@gmail.com>
2022-12-03 22:25:48 +0100

    more reducer tests

--- nodeworks/ecs/reducer.lua
+++ nodeworks/ecs/reducer.lua
@@ -1,3 +1,5 @@
+local nw = require "nodeworks"
+
 local Record = class()
 
 function Record.create()
@@ -12,22 +14,30 @@ function Record.create()
 end
 
 function Record:find(alias)
+    return self.alias[alias]
+end
+
+function Record:maybe_find(alias)
     local node = self.alias[alias]
     return node and nw.just(node) or nw.empty()
 end
 
-function Record:get_info(node)
+function Record:maybe_info(node)
     local info = self.info[node]
     return info and nw.just(info) or nw.empty()
 end
 
-function Record:info_from_alias(alias)
+function Record:maybe_info_from_alias(alias)
     return self
-        :find(alias)
-        :and_then(function(node) return self:get_info(node) end)
+        :maybe_find(alias)
+        :and_then(function(node) return self:maybe_info(node) end)
 end
 
 local Reducer = class()
+
+function Reducer.create()
+    return setmetatable({}, Reducer)
+end
 
 local function format_args(record, arg, ...)
     if arg == nil then
@@ -38,17 +48,19 @@ local function format_args(record, arg, ...)
     end
 end
 
-function Reducer:_invoke(state, record, f, ...)
-    local state, info, next_actions = f(
-        state:copy(), format_args(record, ...)
-    )
+local function invoke_action(state, record, f, ...)
+    return f(state, format_args(record, ...))
+end
+
+function Reducer:_invoke(state, record, action)
+    local state, info, next_actions = invoke_action(state, record, unpack(action))
 
     record.info[action] = info
     if action.alias then record.alias[action.alias] = action end
 
     for _, a in ipairs(next_actions or list()) do
         record.tree:link(action, a)
-        state = self:_invoke(state, record, unpack(a))
+        state = self:_invoke(state, record, a)
     end
 
     return state
@@ -57,13 +69,13 @@ end
 function Reducer:__call(state, action)
     local record = Record.create()
 
-    local final_state = self:_invoke(state, record, unpack(action))
+    local final_state = self:_invoke(state, record, action)
 
-    if self._on_action() then
-        self._on_action(action, final_state, record)
-    end
+    self.on_action(action, final_state, record)
 
     return final_state, record
 end
+
+function Reducer.on_action() end
 
 return Reducer
--- test/ecs/reducer.lua
+++ test/ecs/reducer.lua
@@ -7,4 +7,63 @@ local component = {}
 
 function component.value(v) return v or 0 end
 
-local function add(a, b) return a + b end
+local function add(state, id, b)
+    state:map(component.value, id, function(a) return a + b end)
+    return state, {value=b}
+end
+
+local function add_and_repeat(state, id, a)
+    local actions = list(
+        {add, id, a, alias="prev_add"},
+        {add, id, function(record)
+            return record
+                :maybe_info_from_alias("prev_add")
+                :map(function(info) return info.value end)
+                :value_or_default(0)
+        end}
+    )
+
+    return state, {}, actions
+end
+
+local function multiply(num, val)
+    return num * val
+end
+
+local function factorial(num, next_val)
+    local actions = list(
+        {multiply, next_val},
+        {factorial, next_val - 1}
+    )
+    if next_val > 0 then return num, {}, actions end
+    return num
+end
+
+T("reducer", function(T)
+    local state = nw.ecs.entity.create()
+    local id = "id"
+    local reducer = nw.ecs.reducer.create()
+
+    state:set(component.value, id, 1)
+
+    T("simple_add", function(T)
+        local next_state, record = reducer(
+            state:copy(), {add, id, 1, alias="foobar"}
+        )
+        T:assert(next_state:get(component.value, id) == 2)
+        T:assert(record:maybe_find("foobar"):has_value())
+        T:assert(record:maybe_info_from_alias("foobar"):has_value())
+    end)
+
+    T("add_and_repeat", function(T)
+        local next_state, record = reducer(
+            state:copy(), {add_and_repeat, id, 1}
+        )
+        T:assert(next_state:get(component.value, id) == 3)
+    end)
+
+    T("recursive", function(T)
+        local num, record = reducer(1, {factorial, 3})
+        T:assert(num == 6)
+    end)
+end)
