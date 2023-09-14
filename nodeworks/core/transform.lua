f86ebefe343aaa397e6d25070f63abfe14553ec6
Sebastian Jensen <s.nesgaard@gmail.com>
2023-09-14 22:42:12 +0200

    transform rework

--- nodeworks/core/transform.lua
+++ nodeworks/core/transform.lua
@@ -25,6 +25,28 @@ function transform.__mul(self, other)
     )
 end
 
+function transform:push()
+    gfx.translate(self.x, self.y)
+    gfx.scale(self.sx, self.sy)
+    return self
+end
+
+function transform:apply_spatial(x, y, w, h)
+    local x, y = self:apply(x, y)
+    local w, h = w * self.sx, h * self.sy
+    local dx = w < 0 and w or 0
+    local dy = h < 0 and h or 0
+    return x + dx, y + dy, math.abs(w), math.abs(h)
+end
+
+function transform:apply(x, y)
+    return self.sx * x + self.x, self.sy * y + self.y
+end
+
+function transform:apply_velocity(vx, vy)
+    return self.sx * vx, self.sy * vy
+end
+
 function transform:translation() return self.x, self.y end
 
 function transform:scale() return self.sx, self.sy end
