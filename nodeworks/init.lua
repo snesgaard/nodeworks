bc527e68d0c93d7afabe4059b4eb0349faaf0752
Sebastian Jensen <s.nesgaard@gmail.com>
2024-03-30 18:24:13 +0100

    die init

--- nodeworks/init.lua
+++ nodeworks/init.lua
@@ -1,10 +1,6 @@
---[[
 return {
     ---@module "vec2"
     vec2 = require(... .. ".core.vec2"),
     ---@module "spatial"
     spatial = require(... .. ".core.spatial")
 }
-]]--
-
-return {a=1, b=2}
--- test/core/test_vec2.lua
+++ test/core/test_vec2.lua
@@ -1,9 +1,9 @@
 ---@module "3rd"
 local third = require "3rd"
+
 ---@module "nodeworks"
 local nw = require "nodeworks"
----@module "vec2"
-local vec2 = require "nodeworks.core.vec2"
+
 
 third.test("foo", function(T)
     
